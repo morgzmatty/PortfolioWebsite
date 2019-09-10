e6e11f1484f53718328d2d43e720c23ee337bb18
Morgan Mattone <mpmkid@gmail.com>
2019-09-10 16:04:12 -0400

    initial commit

---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import history from "./history";
+import App from "./app";
+
+// establishes socket connection
+import "./socket";
+
+ReactDOM.render(
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
